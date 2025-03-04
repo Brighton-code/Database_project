eff776013f52086f4e849272ba7b9122979268d4
bright <Brighton@vanrouendal.nl>
2025-03-04 20:40:19 +0100

    Testing database with mysql

--- Database_project/Program.cs
+++ Database_project/Program.cs
@@ -7,4 +7,14 @@
             Console.WriteLine("Hello, World!");
         }
     }
+
+    public class Database
+    {
+        public void ConfigureServices(IServiceProvider services)
+        {
+            string connectionString = "server=localhost;user=root;password=password;database=test;";
+
+            var serviceVersion = new MySqlServerVersion();
+        }
+    }
 }
---
+++ Mysql_project/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mysql_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AppDbContext context = new AppDbContext())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+    }
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseMySQL("server=localhost;database=test_database;user=root;password=password;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Name = "Admin", Password = "12345678" }
+            );
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
