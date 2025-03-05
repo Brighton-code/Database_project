f1f0a0da012b612bc5dad9522a98988474620294
Brighton <s1211945@student.windesheim.nl>
2025-03-05 08:53:41 +0100

    a

--- Database_project/Program.cs
+++ Database_project/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Database_project
+﻿using Microsoft.EntityFrameworkCore;
+using Database_project.Models;
+
+namespace Database_project
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            using (UserContext context = new UserContext())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+    }
+
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            optionsBuilder.UseSqlite(@$"Data Source={documentsPath}\Testing.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Name = "John", Password = "12345678" },
+                new User { Id = 2, Name = "Jane", Password = "password" },
+                new User { Id = 3, Name = "Amber", Password = "12345678" }
+            );
         }
     }
 
