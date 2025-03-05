ddcfb70c006a73f41df3b7965d9a0b210a7eb5c4
Brighton <s1211945@student.windesheim.nl>
2025-03-05 08:54:48 +0100

    a

--- Database_project/Program.cs
+++ Database_project/Program.cs
@@ -8,41 +8,6 @@ namespace Database_project
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            using (UserContext context = new UserContext())
-            {
-                context.Database.EnsureCreated();
-            }
-        }
-    }
-
-    public class UserContext : DbContext
-    {
-        public DbSet<User> Users { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            optionsBuilder.UseSqlite(@$"Data Source={documentsPath}\Testing.db");
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            //base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<User>().HasData(
-                new User { Id = 1, Name = "John", Password = "12345678" },
-                new User { Id = 2, Name = "Jane", Password = "password" },
-                new User { Id = 3, Name = "Amber", Password = "12345678" }
-            );
-        }
-    }
-
-    public class Database
-    {
-        public void ConfigureServices(IServiceProvider services)
-        {
-            string connectionString = "server=localhost;user=root;password=password;database=test;";
-
-            var serviceVersion = new MySqlServerVersion();
         }
     }
 }
